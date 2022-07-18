0573f9f8cf02122511cd52aa5c7e2cc6e2cda76b
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-07-17 23:49:24 -0700

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,19 @@
-require 'pry'
+require "pry"
 
 class String
-
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    return self.split(/[.?!]/).reject { |c| c.empty? }.count
   end
 end
